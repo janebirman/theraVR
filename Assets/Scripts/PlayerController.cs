8b8e10c3c93e593952ed1d8de8d90274de0659b0
janebirman <janebirman@gmail.com>
2017-11-04 22:24:13 -0500

    finally

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+    }
+    //ontriggerenter is first called by Unity when our player game object touches a trigger collider
+    void OnTriggerEnter(Collider other)
+    {
+        //other is a reference to the collider we have touched
+        //Destroy(other.gameObject);
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+    }
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >=10)
+        {
+            winText.text = "you wonnn";
+        }
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    public GameObject target;
+    public float rotationSpeed = 5;
+    public float moveSpeed = 0.1f;
+   // private Rigidbody rb;
+   // private Vector3 mousePosition;
+
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+
+       Vector3 vectorToTarget = target.transform.position - transform.position;
+        
+    }
+}
+
+
